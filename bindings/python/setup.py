4cf13196da63a4b45d7aaf0e467b6cf8c8eef0b2
Stanley Gu <stanleygu@gmail.com>
2014-03-27 21:28:56 +0000

    Added setup.py

---
+++ bindings/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sbml2matlab',
+      version='1.2.0',
+      description='An SBML to MATLAB translator',
+      author='Stanley Gu, Lucian Smith',
+      author_email='stanleygu@gmail.com, lucianoelsmitho@gmail.com',
+      url='https://github.com/stanleygu/sbml2matlab',
+      packages=['sbml2matlab'],
+      )
