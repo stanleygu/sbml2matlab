3b900fe9850e72b6cd4e2039748d406d2f1120a1
Stanley Gu <stanleygu@gmail.com>
2014-03-30 04:24:15 +0000

    Updated to add the proper files

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -3,10 +3,14 @@
 from distutils.core import setup
 
 setup(name='sbml2matlab',
-      version='1.2.0',
+      version='1.2.1',
       description='An SBML to MATLAB translator',
       author='Stanley Gu, Lucian Smith',
       author_email='stanleygu@gmail.com, lucianoelsmitho@gmail.com',
       url='https://github.com/stanleygu/sbml2matlab',
       packages=['sbml2matlab'],
-      )
+      package_data={
+              # add dll, won't hurt unix, not there anyway
+              "sbml2matlab": ["_libsbml2matlab.pyd", "*.dll", "*.txt",
+                              "*.lib"]
+      })
